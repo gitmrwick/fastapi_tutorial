491637c8a362d70afa26f799d8ebcbeb218d58e5
Chadwick <chadwick.employment@gmail.com>
2024-08-23 14:53:04 +0200

    request body with params

--- main.py
+++ main.py
@@ -88,3 +88,8 @@ async def create_item(item: Item):
         price_with_tax = item.price + (item.price * (item.tax/100))
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
